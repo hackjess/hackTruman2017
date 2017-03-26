09b2612723ec5db5b365f7c209f8b960b7371588
Gabby Ortman <ortmangl@gmail.com>
2017-03-26 10:50:30 -0500

    submission commit for procrasti-bot

---
+++ submissions/procrasti-bot/procrasti_bot_messages.py
@@ -0,0 +1,26 @@
+automated_messages = ["PROCRASTINATION TIP: Break your work into small, manageable tasks then create deadlines for these tasks.",
+					"PROCRASTINATION TIP: Put yourself in an environment conducive to working.",
+					"PROCRASTINATION TIP: Block distracting websites and notifications on your laptop, phone, and tablet.",
+					"PROCRASTINATION TIP: Get off Twitter.",
+					"Hey, are you supposed to be working on something? Get to it! I believe in you.", 
+					"Get back to work.", 
+					"Productivity App Recommendation - Forest: https://www.forestapp.cc/en/", 
+					"Chrome Extension Recommendation - StayFocusd: https://chrome.google.com/webstore/detail/stayfocusd/laankejkbhbdhmipfmgcngdelahlfoji?hl=en", 
+					"I hope this is just a quick Twitter check! Get back to work!", 
+					"Don't put off until tomorrow what you can do today",
+					"Tomorrow is always the busiest day of the week. Get back to work."]
+
+warning_messages = ["I've noticed that you've been tweeting a lot, get back to work!", 
+					"Hey! Isn't there something you should be doing right now?", 
+					"Why are you on Twitter again? Get off!",
+					"That hot take can wait another hour. Get some work done.", 
+					"Stop lying to yourself and get work done", 
+					"Do you really need a bot to tell you to stop messing around?", 
+					"If there is a sign from God, this is it. Get back to work.", 
+					"Hey, cutie, get back to work."]
+
+rando_messages = ["Hey there, pal, Procrasti-Bot here to remind you to keep working.", 
+				"What else could you be doing other than procrastinating?",
+				"Don't mean to be a bother, but maybe get back to work? <3", 
+				"You can do it! Get off Twitter and get it done!", 
+				"Stop procrastinating! You can do it!"]
